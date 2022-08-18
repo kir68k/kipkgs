4c17025b4b7a95a0ac4d10dd3e3711a554206b8a
Lapic <lapic@kirinsst.xyz>
2022-08-18 18:11:29 +0200

    aaa

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,6 @@ with super; rec {
   tokyonight-gtk = callPackage ./tokyonight-gtk.nix {};
   tokyonight-kvantum = callPackage ./tokyonight-kvantum.nix {};
   nerdfetch = callPackage ./nerdfetch.nix {};
+  spotify-adblock = callPackage ./spotify-adblock.nix {};
+  spotify-deb = callPackage ./spotify-deb.nix {};
 }
